f012debd87244a456ed489370f129ddb8cc4be00
AFeralMountainGoatBB <brendanbrence@gmail.com>
2021-04-19 19:44:54 -0400

    added rows, resources

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { button, useEffect, useMemo, useState, useContext, useRef } from "react";
-import Button from '@material-ui/core/Button';
-import GameMaster from './GameMaster';
-import InfoGrid from './components/InfoGrid.js';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import IconButton from '@material-ui/core/IconButton';
-import AddIcon from '@material-ui/icons/Add';
-import FaceIcon from '@material-ui/icons/Face'
+import "./App.css";
+import React, {
+  button,
+  useEffect,
+  useMemo,
+  useState,
+  useContext,
+  useRef,
+} from "react";
+import Button from "@material-ui/core/Button";
+import GameMaster from "./GameMaster";
+import InfoGrid from "./components/InfoGrid.js";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import FaceIcon from "@material-ui/icons/Face";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,80 +23,208 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(1),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
 
 function App() {
   const classes = useStyles();
-  const [foodValue, setfoodValue] = useState(1);
+  const [foodValue, setFoodValue] = useState(1);
+  const [woodValue, setWoodValue] = useState(0);
+  const [stoneValue, setStoneValue] = useState(0);
+  const [metalValue, setMetalValue] = useState(0);
   const foodValueRef = useRef(foodValue);
-  foodValueRef.current=foodValue;
-  const [peasantValue, setPeasantValue] = useState({value:0, initialPrice:1, currentPrice:1,
-    foodIncome:1, priceIncreaseModifier:1.07});
+  foodValueRef.current = foodValue;
+
+  const [fieldValue, setFieldValue] = useState({
+    value: 0,
+    initialPrice: 1,
+    currentPrice: 1,
+    foodMultiplier: 1,
+    priceIncreaseModifier: 1.07,
+  });
+  const [forestValue, setForestValue] = useState({
+    value: 0,
+    initialPrice: 1,
+    currentPrice: 1,
+    woodMultiplier: 1,
+    priceIncreaseModifier: 1.07,
+  });
+  const [outcropValue, setOutcropValue] = useState({
+    value: 0,
+    initialPrice: 1,
+    currentPrice: 1,
+    stoneMultiplier: 1,
+    priceIncreaseModifier: 1.07,
+  });
+  const [prospectValue, setDepositValue] = useState({
+    value: 0,
+    initialPrice: 1,
+    currentPrice: 1,
+    metalMultiplier: 1,
+    priceIncreaseModifier: 1.07,
+  });
+
+  const [peasantValue, setPeasantValue] = useState({
+    value: 0,
+    initialPrice: 1,
+    currentPrice: 1,
+    foodIncome: 1,
+    priceIncreaseModifier: 1.07,
+  });
   const peasantValueRef = useRef(peasantValue);
-  peasantValueRef.current=peasantValue;
+  peasantValueRef.current = peasantValue;
 
-  const [tickingBool, setTickBool]=useState(false);
+  const [tickingBool, setTickBool] = useState(false);
 
-  function handleBuyPeasants(){
+  function handleBuyPeasants() {
     var currentPeasantCost = peasantValueRef.current.currentPrice;
     var currentFoodValue = foodValueRef.current;
-   // console.log("handle click", currentFoodValue, currentPeasantCost);
-    if(currentFoodValue>=currentPeasantCost)
-    {
-      var newFoodValue = currentFoodValue-currentPeasantCost;
-     // console.log("yes food value greater");
-      var newPeasantValue = peasantValueRef.current.value+1;
-      var newPeasantCost=Math.ceil(peasantValueRef.current.currentPrice * peasantValueRef.current.priceIncreaseModifier);
-      setfoodValue(newFoodValue);
-      setPeasantValue({...peasantValue, value:newPeasantValue, currentPrice:newPeasantCost});
+    // console.log("handle click", currentFoodValue, currentPeasantCost);
+    if (currentFoodValue >= currentPeasantCost) {
+      var newFoodValue = currentFoodValue - currentPeasantCost;
+      // console.log("yes food value greater");
+      var newPeasantValue = peasantValueRef.current.value + 1;
+      var newPeasantCost = Math.ceil(
+        peasantValueRef.current.currentPrice *
+          peasantValueRef.current.priceIncreaseModifier
+      );
+      setFoodValue(newFoodValue);
+      setPeasantValue({
+        ...peasantValue,
+        value: newPeasantValue,
+        currentPrice: newPeasantCost,
+      });
     }
-    
-    if (!tickingBool)
-    {
+
+    if (!tickingBool) {
       constantTick();
       setTickBool(true);
     }
   }
 
-  function constantTick(){
-   // setPeasantValue(peasantValueRef.current+1);
+  function constantTick() {
+    // setPeasantValue(peasantValueRef.current+1);
     console.log("tick");
     calculateIncome();
     setTimeout(constantTick, 1000);
     // setTimeout(()=>{setPeasantValue(peasantValue+1)}, 1000);
   }
-  
-  function calculateIncome()
-  {
-    //calc income for peasants
-    peasantIncome();
+
+  function calculateIncome() {
+    //calc income for food
+    foodIncome();
+    //woodIncome();
+   // stoneIncome();
+    //metalIncome();
   }
-  function peasantIncome()
-  {
-    var foodIncome = peasantValueRef.current.foodIncome * peasantValueRef.current.value;
-    setfoodValue(foodValueRef.current+foodIncome);
+  function foodIncome() {
+    var foodIncome =
+      peasantValueRef.current.foodIncome * peasantValueRef.current.value;
+    setFoodValue(foodValueRef.current + foodIncome);
+  }
+  function woodIncome() {
+    var foodIncome =
+      peasantValueRef.current.foodIncome * peasantValueRef.current.value;
+    setFoodValue(foodValueRef.current + foodIncome);
+  }
+  function stoneIncome() {
+    var foodIncome =
+      peasantValueRef.current.foodIncome * peasantValueRef.current.value;
+    setFoodValue(foodValueRef.current + foodIncome);
+  }
+  function metalIncome() {
+    var foodIncome =
+      peasantValueRef.current.foodIncome * peasantValueRef.current.value;
+    setFoodValue(foodValueRef.current + foodIncome);
   }
 
   return (
     <div className="App">
-       {/* <InfoGrid value1={peasantValue} value2={foodValue} value3={valuethree} ></InfoGrid> */}
+      {/* <InfoGrid value1={peasantValue} value2={foodValue} value3={valuethree} ></InfoGrid> */}
       {/* <GameMaster/> */}
       <div className={classes.root}>
-      <Grid container spacing={5}>
-        <Grid item xs={3}>
-        <Paper className={classes.paper}>Food: {foodValue}</Paper>
-        </Grid> 
-        <Grid item xs={3}>
-        <Paper className={classes.paper}><FaceIcon/> Peasants: {peasantValue.value}<IconButton onClick={() => { handleBuyPeasants() }}><AddIcon/></IconButton></Paper>
+        <Grid container spacing={5}>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Food: {foodValue}</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Wood: {woodValue}</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Stone: {stoneValue}</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Metal: {metalValue}</Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={3}>
+
+        <Grid
+          container
+          spacing={10}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <FaceIcon /> Peasants: {peasantValue.value}
+              <IconButton
+                onClick={() => {
+                  handleBuyPeasants();
+                }}
+              >
+                <AddIcon />
+              </IconButton>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={3}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Field: {fieldValue.value}</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Forest: {forestValue.value}</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Outcrop: {outcropValue.value}</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Prospect: {prospectValue.value}</Paper>
+          </Grid>
+          </Grid>
+
+          <Grid
+          container
+          spacing={3}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Farmers: {fieldValue.value}</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Woodsmen: {forestValue.value}</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Quarrymen: {outcropValue.value}</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>Prospector: {prospectValue.value}</Paper>
+          </Grid>
+          </Grid>
+        {/* <Grid item xs={3}> */}
         {/* <Paper className={classes.paper}>itemThree {value3}</Paper> */}
-        </Grid>
-      </Grid>
-    </div>
+        {/* </Grid> */}
+      </div>
     </div>
   );
 }
